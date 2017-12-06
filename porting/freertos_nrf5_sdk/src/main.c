4417a34d21b2f2f1b0e8663922bd3a259be5224c
Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
2017-12-06 10:36:40 +0100

    porting: Add FreeRTOS port (based on nRF5 SDK)
    This patch adds port of Nimble stack to FreeRTOS based on nRF5 SDK.
    It's split into 'common' part which can be used a base for porting to
    any other platform and 'freertos_nrf5_sdk' which has FreeRTOS-specific
    files and makefile.
    
    Building and flashing is similar to other demo apps from nRF5 SDK:
    $ make -C porting/freertos_nrf5_sdk
    
    The resulting image can be flashed on nRF52 DK:
    $ make -C porting/freertos_nrf5_sdk flash
    
    Location of nRF5 SDK is set by NRF5_SDK_ROOT variable. By default it is
    set to 'nrf5_sdk' subdirectory so a symlink would work just fine:
    $ ln -s <x>/nRF5_SDK_12.3.0_d7731ad porting/freertos_nrf5_sdk/nrf5_sdk

---
+++ porting/freertos_nrf5_sdk/src/main.c
@@ -0,0 +1,91 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include "FreeRTOS.h"
+#include "task.h"
+#include "bsp.h"
+#include "nordic_common.h"
+#include "nrf_drv_clock.h"
+#include "sdk_errors.h"
+#include "app_error.h"
+#include "bsp/cmsis_nvic.h"
+
+int main(void)
+{
+    ret_code_t err_code;
+
+    /* Relocate ISR vector from flash to RAM */
+    NVIC_Relocate();
+
+    /* Initialize clock driver for better time accuracy in FREERTOS */
+    err_code = nrf_drv_clock_init();
+    APP_ERROR_CHECK(err_code);
+
+    /* Configure LED-pins as outputs */
+    bsp_board_leds_init();
+    bsp_board_leds_off();
+
+    /* Activate deep sleep mode */
+    SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;
+
+    void start_nimble(void);
+    start_nimble();
+
+    /* Start FreeRTOS scheduler. */
+    vTaskStartScheduler();
+
+    while (true)
+    {
+        /* FreeRTOS should not be here... FreeRTOS goes back to the start of stack
+         * in vTaskStartScheduler function. */
+    }
+}
+
+void prvGetRegistersFromStack( uint32_t *pulFaultStackAddress )
+{
+/* These are volatile to try and prevent the compiler/linker optimising them
+away as the variables never actually get used.  If the debugger won't show the
+values of the variables, make them global my moving their declaration outside
+of this function. */
+volatile uint32_t r0 __attribute__((unused));
+volatile uint32_t r1 __attribute__((unused));
+volatile uint32_t r2 __attribute__((unused));
+volatile uint32_t r3 __attribute__((unused));
+volatile uint32_t r12 __attribute__((unused));
+volatile uint32_t lr __attribute__((unused)); /* Link register. */
+volatile uint32_t pc __attribute__((unused)); /* Program counter. */
+volatile uint32_t psr __attribute__((unused));/* Program status register. */
+
+    r0 = pulFaultStackAddress[ 0 ];
+    r1 = pulFaultStackAddress[ 1 ];
+    r2 = pulFaultStackAddress[ 2 ];
+    r3 = pulFaultStackAddress[ 3 ];
+
+    r12 = pulFaultStackAddress[ 4 ];
+    lr = pulFaultStackAddress[ 5 ];
+    pc = pulFaultStackAddress[ 6 ];
+    psr = pulFaultStackAddress[ 7 ];
+
+    /* When the following line is hit, the variables contain the register values. */
+    for( ;; );
+}
+
+/* The prototype shows it is a naked function - in effect this is just an
+assembly function. */
+void HardFault_Handler( void ) __attribute__( ( naked ) );
+
+/* The fault handler implementation calls a function called
+prvGetRegistersFromStack(). */
+void HardFault_Handler(void)
+{
+    __asm volatile
+    (
+        " tst lr, #4                                                \n"
+        " ite eq                                                    \n"
+        " mrseq r0, msp                                             \n"
+        " mrsne r0, psp                                             \n"
+        " ldr r1, [r0, #24]                                         \n"
+        " ldr r2, handler2_address_const                            \n"
+        " bx r2                                                     \n"
+        " handler2_address_const: .word prvGetRegistersFromStack    \n"
+    );
+}
